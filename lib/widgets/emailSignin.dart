bd4951c6075c1f213887492984e81023ad65f2ee
ping5721 <57811102+ping5721@users.noreply.github.com>
2020-10-10 00:15:37 +0700

    email form validate

--- lib/widgets/emailSignin.dart
+++ lib/widgets/emailSignin.dart
@@ -1,7 +1,14 @@
+
 import 'package:flutter/material.dart';
 
 import 'package:flutter_signin_button/flutter_signin_button.dart';
 
+
 Widget buildButtonEmail() {
-  return SignInButton(Buttons.Email, onPressed: () {});
+  return SignInButton(
+    Buttons.Email,
+    onPressed: () {
+      print('pressed');
+    },
+  );
 }
